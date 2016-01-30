0734dc0c3a062e369892b8afa860a2c68862903b
mnraogit <mnrbhav@yahoo.com>
2016-01-30 16:26:56 -0700

    intial commit

---
+++ conditional.py
@@ -0,0 +1,9 @@
+x=7
+day="Friday"
+if x<10:
+    print("That makes sense" +day+". what a great day.")
+else:
+        print("I don't think this will happen")
+        
+        
+        
---
+++ iteration.py
@@ -0,0 +1,7 @@
+books=["hi", "hello", "you"]
+nums = range (100)
+letters = ["i","b","c","d","e"]
+
+for l in letters:
+    print(l)
+        
